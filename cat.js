552d2c300808dbcf1dd64b95ae0e9b75f06a46cc
vinh.tl0502@gmail.com <vinh.tl0502@gmail.com>
2020-04-21 22:34:09 +0700

    Eat method for cat

--- cat.js
+++ cat.js
@@ -2,6 +2,10 @@ function cat() {
     this.stomach=name;
     this.dead=false;
 }
+
+CaretPosition.prototype.eat=function (mouse){
+    this.stomach.push(mouse);
+};
 mouse.prototype.die=function () {
     this.dead=true;
 }
